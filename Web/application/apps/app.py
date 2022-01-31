1801470a5968675b5716acc3343a266dcd0ff551
sai <sai14790085178@163.com>
2022-01-31 21:20:14 +0800

    feat:use flask + html + pymsql process

---
+++ Web/application/apps/app.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template,url_for
+from flask import request
+from pymysql import *
+
+app = Flask(__name__,static_folder='.',static_url_path='',template_folder='./templates')
+
+@app.route('/login',methods=['POST'])
+def login():
+    account=request.form['account']
+    passwd=request.form['passwd']
+
+    print("account is " + account + "\tpasswd is " + passwd)
+    if account == 'admin' and passwd == '123456':
+        conn = connect(host='localhost',port=3306,user='root',password='123456',database='NBIOT',charset='utf8')
+        cursor = conn.cursor()
+        sql = "select * from NBDEV order by id desc limit 100"
+        cursor.execute(sql)
+        nbdev = cursor.fetchall()
+        return render_template('login.html',nbdev=nbdev)
+    else:
+        return render_template('index.html')
+
+
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
+
---
+++ Web/application/apps/static/index.js
@@ -0,0 +1,12 @@
+function oeye(){
+        document.getElementById('owl').style.background='url("./static/image/openeye.png") no-repeat'
+}
+
+function ceye(){
+    document.getElementById('owl').style.background='url("./static/image/closeeye.png") no-repeat'
+}
+
+
+                                                                                      
+                                                                                    
+                                                                                    
